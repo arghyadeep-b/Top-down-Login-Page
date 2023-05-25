e94c687a7eab5901302e93cc59b2f33dbdccc165
Abhishek Vaidya <abhishekvaidya.work@gmail.com>
2023-05-26 00:47:24 +0530

    auth added

---
+++ src/components/socialContainer/Firebase.js
@@ -0,0 +1,31 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider ,FacebookAuthProvider,TwitterAuthProvider} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyBYASVHt897CzvHQDrbOW6EPJlad9GYqdQ",
+  authDomain: "hefork-7542d.firebaseapp.com",
+  projectId: "hefork-7542d",
+  storageBucket: "hefork-7542d.appspot.com",
+  messagingSenderId: "910665598043",
+  appId: "1:910665598043:web:7983c4cb5af4e1a5b7eb7e",
+  measurementId: "G-18V4V76V13"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+
+const ga = new GoogleAuthProvider(app);
+const fa = new FacebookAuthProvider(app);
+const ta = new TwitterAuthProvider(app);
+
+const auth=getAuth(app);
+
+const analytics = getAnalytics(app);
+
+export{auth,ga,fa,ta};
+
+
+
+
--- src/components/socialContainer/SocialContainer.js
+++ src/components/socialContainer/SocialContainer.js
@@ -1,13 +1,85 @@
 import './SocialContainer.css'
+import React, { useEffect, useState} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookF, faGoogle, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import {auth,ga,fa,ta} from "./Firebase"
+import {signInWithPopup} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
 
-export function SocialContainer() {
+
+
+
+   export function SocialContainer() {
+    const [isActive, setIsActive] = useState(false);
+    const[value,setValue]=useState('')
+  
+  
+  
+  
+  
+  //googleauth
+    
+    const googleClick =()=>{
+  
+      signInWithPopup(auth,ga).then((data)=>{
+         setValue(data.user.email)
+         localStorage.setItem('email',data.user.email)
+      })
+     }
+     useEffect(()=>{
+         setValue(localStorage.getItem('email'))
+     })
+  
+  
+  
+  
+  
+  
+     //facebookauth
+     const facebookClick =()=>{
+   
+       signInWithPopup(auth,fa).then((data)=>{
+          setValue(data.user.email)
+          localStorage.setItem('email',data.user.email)
+       })
+      }
+      useEffect(()=>{
+          setValue(localStorage.getItem('email'))
+      })
+  
+  
+  
+  
+  
+  
+      //twitterauth
+      const twitterClick =()=>{
+   
+        signInWithPopup(auth,ta).then((data)=>{
+           setValue(data.user.email)
+           localStorage.setItem('email',data.user.email)
+        })
+       }
+       useEffect(()=>{
+           setValue(localStorage.getItem('email'))
+       })
+  
+       
+
     return (
         <div className="social-container">
-            <button><a href="#" className="social"><FontAwesomeIcon icon={faFacebookF} /></a></button>
-            <button><a href="#" className="social"><FontAwesomeIcon icon={faGoogle} /></a></button>
-            <button><a href="#" className="social"><FontAwesomeIcon icon={faTwitter} /></a></button>
+            <button onClick={facebookClick} className="social"><FontAwesomeIcon icon={faFacebookF} /></button>
+
+
+
+            <button onClick={googleClick} className="social"><FontAwesomeIcon icon={faGoogle} /></button>
+
+
+            <button  onClick={facebookClick} className="social"><FontAwesomeIcon icon={faTwitter} /></button>
         </div>
-    )
+    );
 }
+
+
+
+
+
