69ac747ca69af647b95987cda5a644f3694d094e
Elgius <114131653+Elgius@users.noreply.github.com>
2023-05-29 12:39:55 +0500

    downloading Json works

--- src/components/accountBox/signupForm.jsx
+++ src/components/accountBox/signupForm.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { useState } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -9,20 +10,59 @@ import {
 } from "./common";
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
+import { downloadFile } from "./downloadFile";
+
+
 
 export function SignupForm(props) {
+
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+
+  const downloadFile = ({ data, fileName, fileType }) => {
+    const blob = new Blob([data], { type: fileType });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a); // append the link to the document body
+    a.click(); // simulate a click event on the link
+    window.URL.revokeObjectURL(url); // revoke the object URL
+    document.body.removeChild(a); // remove the link from the document body
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const jsonString = JSON.stringify(formData);
+
+    downloadFile({
+      data: jsonString,
+      fileName: "form-data.json",
+      fileType: "text/json"
+    })
+  }
+
+
+
   const { switchToSignin } = useContext(AccountContext);
 
   return (
     <BoxContainer>
-      <FormContainer>
-        <Input type="text" placeholder="Full Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
-        <Input type="password" placeholder="Confirm Password" />
+      <FormContainer onSubmit={handleSubmit}>
+        <Input type="text" placeholder="Full Name" value={formData.fullName} onChange={(e) => setFormData({ ...formData, fullName: e.target.value })} />
+        <Input type="email" placeholder="Email" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+        <Input type="password" placeholder="Password" value={formData.password} onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
+        <Input type="password" placeholder="Confirm Password" value={formData.confirmPassword} onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })} />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit">Signup</SubmitButton>
+      <SubmitButton type="submit" onClick={handleSubmit}>Signup</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Already have an account?
---
+++ src/components/jsonify/scraper.js
@@ -0,0 +1,12 @@
+export function Scraper(props){
+    const {formData} = props;
+
+    console.log("this is the scraper running", formData)
+
+    return(
+        <div>
+            <h1>Scraper</h1>
+        </div>
+
+    )
+}
