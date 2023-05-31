7e50ab9ac2e04f4b8dfc9983c4f0d1e61a58ab3a
Elgius <114131653+Elgius@users.noreply.github.com>
2023-05-31 10:29:34 +0500

    Login Form json downloader

--- src/components/accountBox/loginForm.jsx
+++ src/components/accountBox/loginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -11,6 +11,40 @@ import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
 
 export function LoginForm(props) {
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+
+  // this exists to download the JSON data so we know its being converted correctly
+  const downloadFile = ({ data, fileName, fileType }) => {
+    const blob = new Blob([data], { type: fileType });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a); // append the link to the document body
+    a.click(); // simulate a click event on the link
+    window.URL.revokeObjectURL(url); // revoke the object URL
+    document.body.removeChild(a); // remove the link from the document body
+  };
+
+
+  const onHandleSubmit = (event) => {
+    event.preventDefault();
+    const jsonString = JSON.stringify(formData);
+
+    downloadFile({
+      data: jsonString,
+      fileName: "form-data.json",
+      fileType: "text/json"
+    })
+
+
+  }
   const { switchToSignup } = useContext(AccountContext);
 
   return (
@@ -22,7 +56,7 @@ export function LoginForm(props) {
       <Marginer direction="vertical" margin={10} />
       <MutedLink href="#">Forget your password?</MutedLink>
       <Marginer direction="vertical" margin="1.6em" />
-      <SubmitButton type="submit">Signin</SubmitButton>
+      <SubmitButton type="submit" onClick={onHandleSubmit}>Signin</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Don't have an accoun?{" "}
